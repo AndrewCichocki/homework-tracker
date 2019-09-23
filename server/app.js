0e3b354d15d462b9951cc888d27adf7d663b7bed
Andrew Cichocki <andrew.cichocki@gmail.com>
2019-09-23 16:18:54 -0400

    Remove unnecessary lines

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const BodyParser = require('koa-bodyparser')
@@ -17,27 +17,20 @@ const app = new Koa()
 const router = new Router()
 
 router
-  .get('/', async (ctx, next) => {
-    ctx.body = 'Welcome to the API server'
-    await next()
-  })
+  .get('/', async (ctx) => ctx.body = 'Welcome to the API server')
   .get('/assignment', async (ctx, next) => {
     try {
       ctx.body = await getAllAssignments()
     } catch (err) {
       ctx.throw(400)
-    } finally {
-      await next()
     }
   })
   .post('/assignment', async (ctx, next) => {
     try {
       const { name, dueDate, status, subject } = ctx.request.body
-     ctx.body = await addAssignment(name, dueDate, status, subject)
+      ctx.body = await addAssignment(name, dueDate, status, subject)
     } catch (err) {
       ctx.throw(400)
-    } finally {
-      await next()
     }
   })
   .put('/assignment/:id', async (ctx, next) => {
@@ -47,8 +40,6 @@ router
       ctx.body = await updateAssignment(id, name, dueDate, status, subject)
     } catch (err) {
       ctx.throw(400)
-    } finally {
-      await next()
     }
   })
   .del('/assignment/:id', async (ctx, next) => {
@@ -57,8 +48,6 @@ router
       ctx.body = await deleteAssignment(id)
     } catch (err) {
       ctx.throw(400)
-    } finally {
-      await next()
     }
   })
 
@@ -75,5 +64,5 @@ app
 
 process.on('SIGTERM', () => {
   dbDisconnect()
-  console.log('Disconnected from DB');
-});
+  console.log('Disconnected from DB')
+})
